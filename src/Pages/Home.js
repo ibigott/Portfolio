6e813993840ef6a1740cbd308fdd6ca2cdf8d51f
ibigott <ibigott@ufl.edu>
2025-03-12 06:05:20 -0400

    Basics set up

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,18 @@
 import './Home.css';
+import headshot from '../Images/headshot.jpg'; 
 
 const Home = () => {
     return (
         <div className='Home'>
-            <h1>Ignacio Bigott</h1>
-            <h2>Software Engineer | CS Graduate</h2>
+            <div className='Hero'>
+                <div>
+                    <h1>Hey, I'm Ignacio Bigott</h1>
+                    <h3>Crafting Code, Creating Experiences</h3>
+                    <h3>Building the Web, One Pixel at a Time</h3>
+                    <h3>Software Engineer | CS Graduate</h3>
+                </div>
+                <img src={headshot} alt='Ignacio Bigott' />
+            </div>
         </div>
     );
 }
